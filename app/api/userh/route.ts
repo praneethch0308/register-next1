721dcd31b28286f070b10840b4f452b1de8f7f76
Sai Praneeth <praneethchilukuri03@gmail.com>
2024-04-17 16:49:38 +0530

    my changes1

--- app/api/userh/route.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-const client = new PrismaClient();
-
-export async function GET() {
-    const user = await client.user.findFirst({where:{}});
-    return Response.json({ firstname: user?.firstName,lastname: user?.lastName, email: user?.email })
-}
